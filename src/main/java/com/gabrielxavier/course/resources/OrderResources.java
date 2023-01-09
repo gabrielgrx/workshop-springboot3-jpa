8f6ca4ad1a8a4c079f05c702955e461ccc4faa85
Gabriel Xavier <gabrielgrx@gmail.com>
2023-01-09 16:31:29 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/gabrielxavier/course/resources/OrderResources.java
@@ -0,0 +1,32 @@
+package com.gabrielxavier.course.resources;
+
+import com.gabrielxavier.course.entities.Order;
+import com.gabrielxavier.course.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResources {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAll() {
+        List<Order> list = orderService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> findById(@PathVariable(value = "id") Long id) {
+        Order obj = orderService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
