d1f76f5bdc6586fcd7997f87bf56a5df1f77f63f
Gabriel Xavier <gabrielgrx@gmail.com>
2023-01-10 14:41:33 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/gabrielxavier/course/entities/Product.java
+++ src/main/java/com/gabrielxavier/course/entities/Product.java
@@ -20,8 +20,11 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    @Transient
-    private Set<Category> categoires = new HashSet<>();
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
 
 
     public Product() {
@@ -75,8 +78,8 @@ public class Product implements Serializable {
         this.imgUrl = imgUrl;
     }
 
-    public Set<Category> getCategoires() {
-        return categoires;
+    public Set<Category> getCategories() {
+        return categories;
     }
 
     @Override
